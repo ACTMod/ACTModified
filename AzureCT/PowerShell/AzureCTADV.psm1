fa85c51d2a0fcc6ac3cffdbf36c9bb160b089f9d
ACTMod <52755266+ACTMod@users.noreply.github.com>
2019-07-10 10:36:34 -0700

    Add files via upload

---
+++ AzureCT/PowerShell/AzureCTADV.psm1
@@ -0,0 +1,29 @@
+#Get public and private function definition files.
+    $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+
+#Dot source the files
+    Foreach($import in @($Public))
+    {
+        Try
+        {
+            . $import.fullname
+        }
+        Catch
+        {
+            Write-Error -Message "Failed to import function $($import.fullname): $_"
+        }
+    }
+
+$ModuleManifest = Test-ModuleManifest -path $PSScriptRoot\AzureCTADV.psd1
+$script:XMLSchemaVersion = ([string]$ModuleManifest.Version.Major) + "." + ([string]$ModuleManifest.Version.Minor)
+
+Export-ModuleMember -Function Get-AzureNetworkAvailability
+Export-ModuleMember -Function Get-IPTrace
+Export-ModuleMember -Function Clear-AzureCTHistory
+Export-ModuleMember -Function Show-AzureCTResults
+Export-ModuleMember -Function Remove-AzureCT
+
+Export-ModuleMember -Function Install-LinkPerformance
+Export-ModuleMember -Function Get-LinkPerformance
+Export-ModuleMember -Function Get-LinkPerformanceAdv
+Export-ModuleMember -Function Get-AzureCT
