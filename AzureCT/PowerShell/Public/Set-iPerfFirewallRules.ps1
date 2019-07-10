b079f480b05b2454a668bffc46d39e06df369368
ACTMod <52755266+ACTMod@users.noreply.github.com>
2019-07-10 10:39:59 -0700

    Add files via upload

---
+++ AzureCT/PowerShell/Public/Set-iPerfFirewallRules.ps1
@@ -0,0 +1,9 @@
+﻿# Must be run as admin
+
+# Turn On ICMPv4
+Try {Get-NetFirewallRule -Name Allow_ICMPv4_in -ErrorAction Stop | Out-Null}
+Catch {New-NetFirewallRule -DisplayName "Allow ICMPv4" -Name Allow_ICMPv4_in -Action Allow -Enabled True -Profile Any -Protocol ICMPv4 | Out-Null}
+
+# Turn On iPerf3
+Try {Get-NetFirewallRule -Name Allow_iPerf3_in -ErrorAction Stop | Out-Null}
+Catch {New-NetFirewallRule -DisplayName "Allow iPerf3" -Name Allow_iPerf3_in -Action Allow -Enabled True -Profile Any -Protocol TCP -LocalPort 5201 | Out-Null}
---
+++ AzureCT/PowerShell/Public/Set-iPerfFirewallRulesAdv.ps1
@@ -0,0 +1,34 @@
+﻿# Must be run as admin
+
+# 1. Evaluate and Set input parameters
+# 2. Initialize
+# 3. Create Log File
+# 4. Open Firewall rules
+    #4.1 Turn On ICMPv4
+    #4.2 Turn On iPerf3
+
+# 1. Evaluate and Set input parameters  
+Param($iPerfFWPort=5201)
+
+# 2. Initialize
+$ToolPath = "C:\ACTTools\"
+$File = $ToolPath + "FW.log"
+$Content = ""
+
+# 3. Create log file
+$Content = "Firewall Rules Created: `r`n`r`n"
+New-Item -Path $File -ItemType "file" -Value $Content 
+
+# 4. Open Firewall rules
+
+    # 4.1 Turn On ICMPv4
+    Try {Get-NetFirewallRule -Name Allow_ICMPv4_in -ErrorAction Stop | Out-Null}
+    Catch {New-NetFirewallRule -DisplayName "Allow ICMPv4" -Name Allow_ICMPv4_in -Action Allow -Enabled True -Profile Any -Protocol ICMPv4 | Out-Null}
+    $Content = "Allow_ICMPv4_in -Action Allow -Enabled True -Profile Any -Protocol ICMPv4"
+    Add-Content -Path $File -Value $Content
+
+    # 4.2 Turn On iPerf3
+    Try {Get-NetFirewallRule -Name Allow_iPerf3_in -ErrorAction Stop | Out-Null}
+    Catch {New-NetFirewallRule -DisplayName "Allow iPerf3" -Name Allow_iPerf3_in -Action Allow -Enabled True -Profile Any -Protocol TCP -LocalPort $iPerfFWPort | Out-Null}
+    $Content = "Allow_iPerf3_in -Action Allow -Enabled True -Profile Any -Protocol TCP -LocalPort " + $iPerfFWPort
+    Add-Content -Path $File -Value $Content
