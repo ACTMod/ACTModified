a9bade1bf6f6e772f87fdc2d7843401e45b769d0
ACTMod <52755266+ACTMod@users.noreply.github.com>
2019-07-11 09:51:27 -0700

    Add files via upload

--- AzureCT/PowerShell/Public/Get-LinkPerformanceAdv.ps1
+++ AzureCT/PowerShell/Public/Get-LinkPerformanceAdv.ps1
@@ -150,9 +150,12 @@
     
     # 4. Open Firewall rules
     If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+    
+        {Write-Host "1st One"
         $cmd = $ToolPath + "Set-iPerfFirewallRulesAdv.ps1 -iPerfFWPort " + $iPerfPort
-        Start-Process powershell -PipelineVariable $iPerfPort -Verb runAs -ArgumentList ("-NoExit ", $cmd)} 
-    Else {Invoke-Expression -Command ($ToolPath + "Set-iPerfFirewallRulesAdv.ps1 iPerfFWPort " + $iPerfPort)} # End If
+        Start-Process powershell -PipelineVariable $iPerfPort -Verb runAs -ArgumentList ("-NoExit ", $cmd)}
+    Else {Write-Host "2nd one"
+    Invoke-Expression -Command ($ToolPath + "Set-iPerfFirewallRulesAdv.ps1 iPerfFWPort " + $iPerfPort)}
 
     # 5. Validate iPerf3 connectivity (two ping)Error Stop
     $FileName = $ToolPath + "\TestPerf.log"
